5b38f970c9a587ae29f63fd6d5549dc316bc323e
jedl <jodltomas6@gmail.com>
2025-09-30 16:21:51 +0200

    uloha 0 init

---
+++ uloha-0-zaklady/wks/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace uloha_0_zaklady_vs
+{
+    internal class Program
+    {
+          static int read()
+          {
+              int output;
+
+              String str = Console.ReadLine();
+
+              while (int.TryParse(str, out output) == false)
+              {
+                  Console.WriteLine("Neplatny vstup");
+                  str = Console.ReadLine();
+              }
+
+              return output;
+          } 
+
+          static void Main(string[] args)
+          {
+              Console.WriteLine("Zadejte pocet studentu: ");
+              int pocetStudentu = read();
+
+              Console.WriteLine("Toto je pocet studentu: ");
+              Console.WriteLine(pocetStudentu);
+          }
+      }
+}
