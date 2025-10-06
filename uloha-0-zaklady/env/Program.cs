4656578746a37dba0522bb44948a874df879774c
Tomas Jodl <jodltomas6@gmail.com>
2025-10-06 06:14:25 +0200

    uloha 0 new project

---
+++ uloha-0-zaklady/env/Program.cs
@@ -0,0 +1,49 @@
+﻿using System.ComponentModel;
+
+namespace uloha_0_zaklady 
+{
+  internal class Program
+  {
+    class ConsoleReader<inputType>
+    {
+      public inputType read(string query)
+      {
+        inputType output;
+
+        Console.Write(query);
+        Console.WriteLine();
+
+        String str = Console.ReadLine();
+
+        TypeConverter converter = TypeDescriptor.GetConverter(typeof(inputType));
+
+        while (converter.IsValid(str) == false)
+        {
+          Console.WriteLine("Neplatny vstup");
+          str = Console.ReadLine();
+        }
+
+        return (inputType)converter.ConvertFromString(str);
+      }
+    }  
+
+    static void Main(string[] args) 
+    {
+      Console.WriteLine("test");
+    }
+  }
+}
+
+//static void consoleRead(string query, Type targetType, out targetType output) {
+//  Console.write(query);
+//  Console.writeLine();
+//  String str = Console.ReadLine();
+//
+//  targetType output;
+// 
+//  while (inputType.TryParse(str, out output) == false)
+//  {
+//      Console.WriteLine("Neplatny vstup");
+//      str = Console.ReadLine();
+//  }
+//}
