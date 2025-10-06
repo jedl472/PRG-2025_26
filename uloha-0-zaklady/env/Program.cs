687c779b3d6c421cdcc310d062825655a2162909
Tomas Jodl <jodltomas6@gmail.com>
2025-10-06 22:56:31 +0200

    uloha 0 hotova

--- uloha-0-zaklady/env/Program.cs
+++ uloha-0-zaklady/env/Program.cs
@@ -4,46 +4,83 @@ namespace uloha_0_zaklady
 {
   internal class Program
   {
-    class ConsoleReader<inputType>
-    {
-      public inputType read(string query)
+    static T consoleReadType<T> (string query) {
+      Console.Write(query);
+
+      string? str = Console.ReadLine();
+
+      TypeConverter converter = TypeDescriptor.GetConverter(typeof(T));
+
+      while (converter.IsValid(str) == false | str == null)
       {
-        inputType output;
-
-        Console.Write(query);
-        Console.WriteLine();
-
-        String str = Console.ReadLine();
-
-        TypeConverter converter = TypeDescriptor.GetConverter(typeof(inputType));
-
-        while (converter.IsValid(str) == false)
-        {
-          Console.WriteLine("Neplatny vstup");
-          str = Console.ReadLine();
-        }
-
-        return (inputType)converter.ConvertFromString(str);
+        Console.Write("Neplatny vstup. " + query);
+        str = Console.ReadLine();
       }
-    }  
+
+      return (T)converter.ConvertFromString(str);
+    }
+
+    class Student {
+      public string jmeno;
+      public uint vek;
+      public float prumernaZnamka;
+      
+      public Student () {
+        jmeno = consoleReadType<string>("jmeno: ");
+        vek = consoleReadType<uint>("vek  : ");
+        prumernaZnamka = consoleReadType<float>("prumerna znamka: ");
+      }
+
+      public void consolePrint() {
+        Console.WriteLine($"{jmeno}({vek}): {prumernaZnamka}");
+      }
+    }
+
+
 
     static void Main(string[] args) 
     {
-      Console.WriteLine("test");
+      int pocetStudentu = consoleReadType<int>("Zadejte pocet studentu: ");
+      
+      List<Student> studenti = new List<Student>();
+      for (int i = 0; i < pocetStudentu; i++)
+      {
+          Console.WriteLine("------ student {0} -------", i);
+          studenti.Add(new Student());
+      }
+
+      while (true) {
+        Console.WriteLine();
+        char userInput = consoleReadType<char>("prikaz: ");
+        
+        switch(userInput) {
+          case 'a':
+            foreach(Student student in studenti) {
+              student.consolePrint();
+            }
+            break;
+          case 'b':
+            foreach(Student student in studenti) {
+              if (student.prumernaZnamka < 2.0) {
+                student.consolePrint();
+              } 
+            }
+            break;
+          case 'c':
+            uint soucetVeku = 0;
+            foreach(Student student in studenti) {
+              soucetVeku += student.vek;
+            }
+            Console.WriteLine("Prumerny vek: {0}", (float)soucetVeku/(float)pocetStudentu);
+            break;
+          case 'd':
+            goto Gin;
+        }
+      }
+
+      Gin:
+      Console.WriteLine("fin");
     }
   }
 }
 
-//static void consoleRead(string query, Type targetType, out targetType output) {
-//  Console.write(query);
-//  Console.writeLine();
-//  String str = Console.ReadLine();
-//
-//  targetType output;
-// 
-//  while (inputType.TryParse(str, out output) == false)
-//  {
-//      Console.WriteLine("Neplatny vstup");
-//      str = Console.ReadLine();
-//  }
-//}
--- uloha-0-zaklady/env/obj/Debug/net9.0/env.AssemblyInfo.cs
+++ uloha-0-zaklady/env/obj/Debug/net9.0/env.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("env")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5b38f970c9a587ae29f63fd6d5549dc316bc323e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4656578746a37dba0522bb44948a874df879774c")]
 [assembly: System.Reflection.AssemblyProductAttribute("env")]
 [assembly: System.Reflection.AssemblyTitleAttribute("env")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
